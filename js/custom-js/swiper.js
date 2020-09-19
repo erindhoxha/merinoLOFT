6951f2c3d0a6c0cde399713e925e8ae1ea2945a3
Erind Hoxha <erind_cbh@hotmail.com>
2020-09-20 00:04:07 +1200

    modified for mobile, added an idea for the shop now

--- js/custom-js/swiper.js
+++ js/custom-js/swiper.js
@@ -1,33 +1,37 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 4,
-    spaceBetween: 30,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // Optional parameters
+  // If we need pagination
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 0,
     },
-        // Optional parameters
-    // If we need pagination
-    breakpoints: {  
-        '0': {
-          slidesPerView: 1,
-          spaceBetween: 0, },
-        '568': {
-        slidesPerView: 2,
-        spaceBetween: 20, },
-        '772': {
-            slidesPerView: 3,
-            spaceBetween: 20,},
-        '1080': {
-            slidesPerView: 4,
-            spaceBetween: 30,},
-      },
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+    568: {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-    // And if we need scrollbar
-    scrollbar: {
-    el: '.swiper-pagination'
+    772: {
+      slidesPerView: 3,
+      spaceBetween: 20,
     },
-  });
+    1080: {
+      slidesPerView: 4,
+      spaceBetween: 0,
+    },
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-pagination",
+  },
+});
